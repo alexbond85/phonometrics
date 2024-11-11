ad5e4741ba9e0d682841c3b8f0119e235d83a764
alexbond <sascha.bondarenko@gmail.com>
2024-11-11 22:54:27 +0100

    integrating whisper into common api

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import io
 import logging
+from functools import lru_cache
 
 import torchaudio  # type: ignore
 
@@ -9,9 +10,10 @@ from fastapi import UploadFile
 from fastapi.responses import JSONResponse
 from transformers import AutoModelForCTC  # type: ignore
 from transformers import AutoProcessor
+from fastapi import Query
 
 from phonometrics.transcription.phonemes.model import TranscriptionModel
-
+from phonometrics.transcription.words.whisper_local import LocalWhisperModel
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -51,6 +53,43 @@ async def transcribe(file: UploadFile = File(...)):
 
     # Return the transcription as JSON
     return JSONResponse(content=transcription)
+
+
+# Cached model loader to ensure each model size is only loaded once
+@lru_cache(maxsize=2)
+def get_model(model_size: str) -> LocalWhisperModel:
+    return LocalWhisperModel(model_size=model_size)
+
+
+@app.post("/transcribe_words")
+async def transcribe_audio(
+    model_size: str = Query("base", enum=["base", "medium"]),
+    file: UploadFile = File(...),
+):
+    # Load
+    transcriber = get_model(model_size)
+    logger.info("Received transcription request.")
+
+    # Read the uploaded file into bytes
+    audio_bytes = await file.read()
+    logger.info("Audio file read into bytes.")
+
+    # Load audio data using torchaudio
+    audio_file = io.BytesIO(audio_bytes)
+    audio_waveform, sample_rate = torchaudio.load(audio_file)
+    logger.info(
+        f"Audio data loaded: sample rate = {sample_rate}, waveform shape = {audio_waveform.shape}"
+    )
+
+    # Transcribe audio
+    transcription = transcriber.transcribe_from_waveform(
+        audio_waveform, sample_rate
+    )
+    logger.info("Transcription completed.")
+
+    # Return the transcription as JSON
+    return JSONResponse(content=transcription)
+
 
 
 @app.get("/health")
