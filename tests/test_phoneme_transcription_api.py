8fa21cf1e79101d4a0ef4430275b00b58de64412
alexbond <sascha.bondarenko@gmail.com>
2024-11-11 16:26:38 +0100

    added config yaml

--- tests/test_phoneme_transcription_api.py
+++ tests/test_phoneme_transcription_api.py
@@ -1,29 +1,38 @@
+from distutils.command.config import config
+from pathlib import Path
+
 import pytest
 import requests
-from pathlib import Path
+import yaml
 
 
 @pytest.mark.integration
 def test_phoneme_api(sample_audio_data):
     # Define the URL for the API endpoint
-    url = "http://localhost:8000/transcribe"
+    with open("config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+    url = config["transcription_service_url"]
     # Determine the current directory and path to the input file
     current_directory = Path(__file__).parent
-    path_to_input_file = current_directory / 'data' / 'billet.mp3'
+    path_to_input_file = current_directory / "data" / "billet.mp3"
     # Check if the file exists
-    assert path_to_input_file.exists(), f"Input file {path_to_input_file} does not exist."
+    assert (
+        path_to_input_file.exists()
+    ), f"Input file {path_to_input_file} does not exist."
     # Prepare the file data for the request
     files = {
-        'file': (
-            'billet.mp3',
-            open(str(path_to_input_file), 'rb'),
-            'audio/wav'
+        "file": (
+            "billet.mp3",
+            open(str(path_to_input_file), "rb"),
+            "audio/wav",
         )
     }
     # Send the POST request
     response = requests.post(url, files=files)
     # Check that the response was successful
-    assert response.status_code == 200, f"Request failed with status code {response.status_code}"
+    assert (
+        response.status_code == 200
+    ), f"Request failed with status code {response.status_code}"
     response_data = response.json()
     # assert
     assert sample_audio_data["transcription"] == response_data["transcription"]
